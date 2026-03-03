fb0b1d1b875d75eed2f010c9bddfa5a67a2fcee1
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2026-03-03 23:40:56 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,12 +15,15 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
-    /**
+/**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Paginator::useBootstrapFive();
+public function boot(): void
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
+}
+
+    
 }
